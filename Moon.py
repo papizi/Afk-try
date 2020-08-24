2577c9fb0fadc058b123201902debdf781a768a8
papizi <69735260+papizi@users.noreply.github.com>
2020-08-24 14:35:53 +0300

    Create Moon.py

---
+++ Moon.py
@@ -0,0 +1,35 @@
+# made by @PapichX
+
+
+from time import sleep
+from userbot.events import register
+
+
+@register(outgoing=True, pattern='^.fl(?: |$)(.*)')
+async def typewriter(typew):
+    await typew.edit("`start loading...`")
+    sleep(1)
+    await typew.edit("🌝")
+    sleep(0.03)
+    await typew.edit("🌚")
+    sleep(0.03)
+    await typew.edit("🌝")
+    sleep(0.03)
+    await typew.edit("🌚")
+    sleep(0.03)
+    await typew.edit("🌝")
+    sleep(0.03)
+    await typew.edit("🌚")
+    sleep(0.03)
+    await typew.edit("🌝")
+    sleep(0.03)
+    await typew.edit("🌚")
+    sleep(0.03)
+    await typew.edit("🌝")
+    sleep(0.03)
+    await typew.edit("🌚")
+    sleep(0.03)
+    await typew.edit("🌝")
+ 
+    sleep(1)
+    await typew.edit("`Done!`")
