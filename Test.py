724223d6dad7e4d220ef1b81cd4ebb5fbe317c50
papizi <69735260+papizi@users.noreply.github.com>
2021-01-03 19:13:22 +0200

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,15 +1,23 @@
-# made by @PapichX
+# by @AliBaBoy
 
+from .. import loader, utils
 
-from time import sleep
-from userbot.events import register
+import logging
+import asyncio
+import time
 
+logger = logging.getLogger(__name__)
 
-@register(outgoing=True, pattern='^.slot(?: |$)(.*)')
-async def typewriter(typew):
-    await typew.edit("Слот 20М")
-    sleep(20.00)
-    await typew.print("Слот 20М")
-    sleep(20.00)
-    await typew.print("Слот 20М")
-    sleep(20.00)
+@loader.tds
+class HACKMod(loader.Module)
+	strings = {"name": "Slot"}
+	def __init__(self):
+		self.name = self.strings["name"]
+	def config_complete(self):
+		pass
+	async def hackcmd(self, message):("Слот 10М")
+			time.sleep(20.0)
+        async def hackcmd(self, message):("Слот 10М")
+  			time.sleep(20.0)
+        async def hackcmd(self, message):("Слот 10М")
+			time.sleep(20.0)
