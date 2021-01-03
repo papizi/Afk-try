43424ba12ed60c97c59d30768fa34b2f176988a8
papizi <69735260+papizi@users.noreply.github.com>
2021-01-03 18:47:24 +0200

    Create Test.py

---
+++ Test.py
@@ -0,0 +1,22 @@
+from time import sleep
+from userbot.events import register
+
+
+@register(outgoing=True, pattern='^.slot(?: |$)(.*)')
+async def typewriter(typew):
+	await conv.send_message ("слот 10М")
+	sleep(20.00)
+	await conv.send_message ("слот 10М")
+	sleep(20.00) 
+	await conv.send_message ("слот 10М")
+	sleep(20.00)
+	await conv.send_message ("слот 10М")
+	sleep(20.00)
+	await conv.send_message ("слот 10М")
+	sleep(20.00)
+	await conv.send_message ("слот 10М")
+	sleep(20.00) 
+	await conv.send_message ("слот 10М")
+	sleep(20.00)
+	await conv.send_message ("слот 10М")
+	sleep(20.00)
